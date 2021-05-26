9e19063b757df93f26a763b4d969cc23181717e4
shinya.kawaguchi <shin.k_9826@ezweb.ne.jp>
2021-05-26 14:41:44 +0900

    Add getAllMessages

--- src/controllers/IndexServlet2.java
+++ src/controllers/IndexServlet2.java
@@ -1,12 +1,17 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Tasks;
+import utils.DButil;
 
 /**
  * Servlet implementation class IndexServlet2
@@ -26,7 +31,11 @@ public class IndexServlet2 extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
+        EntityManager em = DButil.createEntityManager();
+
+        List<Tasks> tasks = em.createNamedQuery("getAllTasks", Tasks.class).getResultList();
+        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+
+        em.close();    }
 
 }
