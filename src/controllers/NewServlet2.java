20008a029f6f97633d5269faa1bab153a2ac8be5
shinya.kawaguchi <shin.k_9826@ezweb.ne.jp>
2021-05-26 15:26:36 +0900

    Add NewServlet.java

--- src/controllers/NewServlet2.java
+++ src/controllers/NewServlet2.java
@@ -1,33 +1,64 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Tasks;
+import utils.DButil;
+
 /**
  * Servlet implementation class NewServlet2
  */
 @WebServlet("/new")
 public class NewServlet2 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public NewServlet2() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DButil.createEntityManager();
+
+        Tasks t = new Tasks();
+
+
+        String content = "user";
+        t.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        t.setCreated_at(currentTime);
+        t.setUpdated_at(currentTime);
+
+
+        em.getTransaction().begin();
+        em.persist(t);
+        em.getTransaction().commit();
+
+
+
+
+
+
+
+
+
+        response.getWriter().append(Integer.valueOf(t.getId()).toString());
+
+        em.close();
+    }
 
 }
